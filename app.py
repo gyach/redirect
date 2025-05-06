cc2718a0c830618c1c368e13de2585cd1bbaa3de
Sergey Gyach <sergey@gyach.ru>
2025-05-06 13:41:43 +0300

    Revert "Переход на пути вместо поддоменов"
    This reverts commit c056f5c45a9de396c832156e11e804d8d6e5b6b9.

--- app.py
+++ app.py
@@ -6,27 +6,27 @@ load_dotenv()
 
 app = Flask(__name__)
 
-# Словарь с маппингом путей на целевые URL
+# Словарь с маппингом поддоменов на целевые URL
 REDIRECTS = {
     'zoom': 'https://us05web.zoom.us/j/81116686762?pwd=yvd6q03psRhRSFELyllk9WSEZ52tpI.1',
     'meet': 'https://meet.google.com/zbq-gnwd-ksh',
     'telemost': 'https://telemost.yandex.ru/j/3170511128',
+    # Добавьте здесь свои поддомены и их целевые URL
 }
 
+@app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
-def redirect_path(path):
-    # Получаем первый сегмент пути
-    first_segment = path.split('/')[0]
+def redirect_subdomain(path):
+    # Получаем поддомен из заголовка Host
+    host = request.host.split('.')
+    if len(host) > 2:  # Проверяем, что это поддомен
+        subdomain = host[0]
+        if subdomain in REDIRECTS:
+            return redirect(REDIRECTS[subdomain], code=301)
     
-    if first_segment in REDIRECTS:
-        return redirect(REDIRECTS[first_segment], code=301)
-    
-    # Если путь не найден, перенаправляем на главную страницу
-    return redirect('https://gyach.ru', code=301)
-
-@app.route('/')
-def home():
-    return redirect('https://gyach.ru', code=301)
+    # Если поддомен не найден или это основной домен, 
+    # можно перенаправить на главную страницу
+    return redirect('https://github.com/gyach', code=301)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000))) 
