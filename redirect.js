e7c618d4dce8e268d0a7ee3f6ef6e9ddeadff3da
Sergey Gyach <sergey@gyach.ru>
2025-05-22 10:00:35 +0300

    Добавление новых редиректов на русском языке в файл redirect.js и обновление CNAME для поддержки нового домена.

--- redirect.js
+++ redirect.js
@@ -2,6 +2,9 @@ const REDIRECTS = {
   zoom: 'https://us05web.zoom.us/j/81116686762?pwd=yvd6q03psRhRSFELyllk9WSEZ52tpI.1',
   meet: 'https://meet.google.com/zbq-gnwd-ksh',
   telemost: 'https://telemost.yandex.ru/j/3170511128',
+  зум: 'https://us05web.zoom.us/j/81116686762?pwd=yvd6q03psRhRSFELyllk9WSEZ52tpI.1',
+  мит: 'https://meet.google.com/zbq-gnwd-ksh',
+  телемост: 'https://telemost.yandex.ru/j/3170511128',
 };
 
 function getPath() {
